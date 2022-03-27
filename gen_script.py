c47a9a28bfb673f63191d00e3083477149747844
Polina Romanchenko <polinaromanchenko2000@gmail.com>
2022-03-28 01:32:49 +0300

    removed trash

--- gen_script.py
+++
@@ -1,8 +0,0 @@
-tasks = []
-for i in range(ord('B'), ord('U') + 1):
-    from_ = chr(i)
-    x = chr(i - 1)
-    print('cd {}/java'.format(from_))
-    print('pwd')
-    print('mv {}.java Solution.java | true'.format(x))
-    print('cd ../..')
