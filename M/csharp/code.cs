519c8ce10d150d15c19b202920a3575dace04271
Ivan Samsonov <hronya@gmail.com>
2024-11-02 15:06:48 +0800

    Add CSharp and Go templates for M

---
+++ M/csharp/code.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+/** Comment it before submitting
+public class Node {
+    public int val;
+    public IList<Node> neighbours;
+
+    public Node() {
+        val = 0;
+        neighbours = new List<Node>();
+    }
+
+    public Node(int _val) {
+        val = _val;
+        neighbours = new List<Node>();
+    }
+
+    public Node(int _val, List<Node> _neighbours) {
+        val = _val;
+        neighbours = _neighbours;
+    }
+}
+**/
+
+public class Solution
+{
+    public static Node CloneGraph(Node node)
+    {
+        // Your code
+        // “ヽ(´▽｀)ノ”
+    }
+}
