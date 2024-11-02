aac584f47bfb124740c28194a7842ebd85063b6a
Ivan Samsonov <hronya@gmail.com>
2024-11-02 15:16:04 +0800

    Add templates for CSharp and Go

---
+++ H/csharp/code.cs
@@ -0,0 +1,19 @@
+/** Comment it before submitting
+public class Node {
+    public int Value { get; private set; }
+    public Node Next { get; set; }
+
+    public Node(int value, Node next)
+    {
+        Value = value;
+        Next = next;
+    }
+}
+**/
+
+public class Solution {
+    public static Node Reverse(Node head, int left, int right) {
+        // Your code
+        // “ヽ(´▽｀)ノ”
+    }
+}
